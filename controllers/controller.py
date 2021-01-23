a652bd919c4cddbced64c9ab3957386e5fadaede
Duarte Elvas <duartecelvas@gmail.com>
2021-01-23 16:21:44 +0000

    Initial commit

---
+++ controllers/controller.py
@@ -0,0 +1,8 @@
+import random
+
+class controller:
+  def __init__(self):
+    pass
+
+  def choose(self,list):
+      return random.choice(list)
---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+import os
+from controllers.controller import controller
+from keep_alive import keep_alive
+
+cont = controller()
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(client.user)
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return 0
+    if message.content.startswith("!choose"):
+        print("#-" * 30)
+        await message.channel.send(cont.choose(message.content.split(" ")[1:]))
+        print(message.author)
+        print(message.content)
+        print("#-" * 30)
+
+
+keep_alive()
+client.run(os.getenv("TOKEN"))  #Secret Stuff
