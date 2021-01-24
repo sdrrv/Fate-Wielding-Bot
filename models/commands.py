354c4bffe99d1ed6d159d3d1dc13cba3038c2dd5
Duarte Elvas <duartecelvas@gmail.com>
2021-01-24 20:01:34 +0000

    Added the model commands

---
+++ models/commands.py
@@ -0,0 +1,8 @@
+class commands:
+    def __init__(self):
+        DG = {
+            "choose":"!fate choose S s2 z3 - This will choose randomly between S,S2 and S3"
+        }
+    
+    def get_commands(self):
+        return self.DG
