937ef8741dabeaa542595264bc6968932608e501
Duarte Elvas <duartecelvas@gmail.com>
2021-01-27 23:57:41 +0000

    Nuke not working

---
+++ models/models.py
@@ -0,0 +1,15 @@
+class models:
+    def __init__(self):
+
+        self.disconnect_phrases=["See ya later ","Smell ya later ","And don´t come back you filthy pig ","You are better dead ","Right up your ass "]
+
+        self.bombed_phrases=["You just got bombed"]
+    
+    def get_commands(self):
+        return self.Dg
+    
+    def get_disconnect_phrases(self):
+        return self.disconnect_phrases
+    
+    def get_bombed_phrases(self):
+        return self.bombed_phrases
