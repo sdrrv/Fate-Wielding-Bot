075af3a5cefb755adc48be108d423247062716f4
Doginainar <72704156+Doginainar@users.noreply.github.com>
2021-02-04 13:55:39 +0000

    Update randomizers.py

--- cogs/randomizers.py
+++ cogs/randomizers.py
@@ -37,7 +37,7 @@ class Randomizers(commands.Cog):
         channel= ctx.author.voice.channel
         #-------------------------------------
         members= [i for i in channel.members]
-        if len(members)<=1:
+        if len(members)<=0:
             await ctx.channel.send(f"You seem to be alone <@{ctx.author.id}>... no one to nuke")
             return 1
         num_to_kick=self.cont.choose_num_between(1,len(members))
@@ -45,7 +45,7 @@ class Randomizers(commands.Cog):
         #-------------------------------------
         voice_client= await self.cont.join(channel)
         self.cont.play(voice_client,"WTF BOOM Sound Byte.mp3")
-        time.sleep(2)
+        time.sleep(3.5)
 
         result= self.cont.get_bombed_phrase()
 
