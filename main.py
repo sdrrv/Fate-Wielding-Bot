8ffa728b72f8164aaf695ec1011134af5bc5e1e4
Duarte Elvas <duartecelvas@gmail.com>
2021-02-10 15:05:16 +0000

    Fix ;)

--- main.py
+++ main.py
@@ -32,10 +32,7 @@ async def on_command_error(ctx, error):
     elif isinstance(error, commands.MemberNotFound):
         await ctx.channel.send(f"Member '`{error.argument}`' not found")
         return
-    elif isinstance(error, commands.ClientException):
-          print("Hello")
-          pass
-
+    
     raise error
 
 @bot.event #On new Server Enter
