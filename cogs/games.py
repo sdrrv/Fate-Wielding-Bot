fa2ebd3c59611541b3e5e9ae9b1649eca2b9fa5c
Duarte Elvas <duartecelvas@gmail.com>
2021-02-11 12:23:58 +0000

    Patch

--- cogs/games.py
+++ cogs/games.py
@@ -52,6 +52,7 @@ class Games(commands.Cog):
             bang = await self.bot.wait_for("message",timeout=30,check=lambda i: ( (i.author == ctx.author or i.author == user ) and (i.content=="!bang")) )       
         except Exception:
             await ctx.channel.send(f"No Shoots... {user.id},{ctx.author.id} you peace loving freaks.")
+            await self.cont.leave(voice_client) #self disconnect
             return
 
         self.cont.play(voice_client,"shoot.wav")
