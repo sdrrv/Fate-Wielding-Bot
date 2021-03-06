2131c03abeb3c042c12972f1414aee39cc3930a6
sdrrv <duartecelvas@gmail.com>
2021-03-06 14:41:01 +0000

    Online Server True

--- main.py
+++ main.py
@@ -8,18 +8,20 @@ from keep_alive import keep_alive
 import json
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix="!fate ",intents = intents)
+bot = commands.Bot(command_prefix="!fate ", intents=intents)
 cont = controller(bot)
 
 admins = cont.getAdmins()
 
-@bot.event #On Ready
+
+@bot.event  # On Ready
 async def on_ready():
     await bot.change_presence(activity=discord.Game("!fate help"))
     print(bot.user)
     print([i.name for i in bot.guilds])
 
-@bot.event #On Error
+
+@bot.event  # On Error
 async def on_command_error(ctx, error):
     print("error")
     cont.debug(ctx)
@@ -33,88 +35,98 @@ async def on_command_error(ctx, error):
         await ctx.channel.send(f"Member '`{error.argument}`' not found")
         return
     elif isinstance(error, commands.MissingRequiredArgument):
-          await ctx.channel.send("You're missing an argument there bud...\nTry `!fate help <command>`")
-          return
+        await ctx.channel.send("You're missing an argument there bud...\nTry `!fate help <command>`")
+        return
     raise error
 
-@bot.event #On new Server Enter
+
+@bot.event  # On new Server Enter
 async def on_guild_join(guild):
     print(f"{guild.name}, Online")
-    with open("./models/leaderBoard.json","r") as f:
-      leaderBoard = json.load(f)
+    cont.debugLogGreen(f"{guild.name}, Online")
+    with open("./models/leaderBoard.json", "r") as f:
+        leaderBoard = json.load(f)
 
-    leaderBoard[str(guild.id)]= {
-      "games":{},
-      "randomizers":{
-        "ban":[]
-      }
+    leaderBoard[str(guild.id)] = {
+        "games": {},
+        "randomizers": {
+            "ban": []
+        }
     }
 
-    with open("./models/leaderBoard.json","w") as f:
-      json.dump(leaderBoard, f, indent=4)
+    with open("./models/leaderBoard.json", "w") as f:
+        json.dump(leaderBoard, f, indent=4)
 
-    general = find(lambda x: (x.name == 'general' or x.name =="geral"),  guild.text_channels)
+    general = find(lambda x: (x.name == 'general' or x.name ==
+                              "geral"),  guild.text_channels)
     if general and general.permissions_for(guild.me).send_messages:
-        await general.send(f"Hello there, `{guild.name}`!\nThank you for adding our bot, we hope you have as mutch fun using it, as we did coding it.\n"\
-          + cont.help())
+        await general.send(f"Hello there, `{guild.name}`!\nThank you for adding our bot, we hope you have as mutch fun using it, as we did coding it.\n"
+                           + cont.help())
+
 
 @bot.event
 async def on_guild_remove(guild):
-  print(f"{guild.name}, Offline")
-  with open("./models/leaderBoard.json","r") as f:
-      leaderBoard = json.load(f)
+    print(f"{guild.name}, Offline")
+    with open("./models/leaderBoard.json", "r") as f:
+        leaderBoard = json.load(f)
 
-  leaderBoard.pop(str(guild.id))
+    leaderBoard.pop(str(guild.id))
 
-  with open("./models/leaderBoard.json","w") as f:
-      json.dump(leaderBoard, f, indent=4)
-  
+    with open("./models/leaderBoard.json", "w") as f:
+        json.dump(leaderBoard, f, indent=4)
+
 #!--------------------------------------------------------------------------------------------------------------------------------------
 
+
 @bot.command(name="about",
-brief="A little about the bot"
-)
+             brief="A little about the bot"
+             )
 async def about(ctx):
     cont.debug(ctx)
     await ctx.channel.send(cont.help())
 
 #!-----------------------------------------------------------------------------------------------------------------------------------------
-@bot.command(name="pull" , hidden=True)
+
+
+@bot.command(name="pull", hidden=True)
 async def pull(ctx):
-  if(ctx.author.id not in admins):
+    if(ctx.author.id not in admins):
         return
-  os.system("git pull https://github.com/sdrrv/Fate-Wielding-Bot.git")
+    os.system("git pull https://github.com/sdrrv/Fate-Wielding-Bot.git")
 
 #!-----------------------------------------------------------------------------------------------------------------------------------------
+
 
 @bot.command(hidden=True)
 async def load(ctx, name):
-  if(ctx.author.id not in admins):
+    if(ctx.author.id not in admins):
         return
-  bot.load_extension(f"cogs.{name}")
-  print(f"{name} Loaded!")
+    bot.load_extension(f"cogs.{name}")
+    print(f"{name} Loaded!")
+
 
 @bot.command(hidden=True)
 async def unload(ctx, name):
-  if(ctx.author.id not in admins):
+    if(ctx.author.id not in admins):
         return
-  bot.unload_extension(f"cogs.{name}")
-  print(f"{name} UnLoaded!")
+    bot.unload_extension(f"cogs.{name}")
+    print(f"{name} UnLoaded!")
+
 
 @bot.command(hidden=True)
-async def reload(ctx,name):
-  if(ctx.author.id not in admins):
-        return 
-  bot.unload_extension(f"cogs.{name}")
-  bot.load_extension(f"cogs.{name}")
-  print(f"{name} ReLoaded!")
+async def reload(ctx, name):
+    if(ctx.author.id not in admins):
+        return
+    bot.unload_extension(f"cogs.{name}")
+    bot.load_extension(f"cogs.{name}")
+    print(f"{name} ReLoaded!")
 
 
-for file in os.listdir("./cogs"): #Will load all COGs
-      if file.endswith(".py"):
-            print("Load "+file)
-            bot.load_extension(f"cogs.{ file[:-3] }")
+for file in os.listdir("./cogs"):  # Will load all COGs
+    if file.endswith(".py"):
+        print("Load "+file)
+        bot.load_extension(f"cogs.{ file[:-3] }")
 
 #!-----------------------------------------------------------------------------------------------------------------------------------------
 keep_alive()
-bot.run(os.getenv("TOKEN"))  #Secret Stuff
+bot.run(os.getenv("TOKEN"))  # Secret Stuff
